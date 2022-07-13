9b89d988ad541ef07fdf8abc528e75f0761bc1c8
maticairo <maticairo@gmail.com>
2022-07-13 11:01:32 -0300

    delete some configs

--- pkg/server/url_mappings.go
+++ pkg/server/url_mappings.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/maticairo/melishows-api/pkg/melishows"
 	"github.com/maticairo/melishows-api/pkg/middlewares"
+	"log"
+	"net/http"
 )
 
 func MapURLs() *mux.Router {
@@ -11,6 +13,12 @@ func MapURLs() *mux.Router {
 	controller := melishows.NewController()
 
 	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hi Mate!")
+	})
+	router.HandleFunc("/_ah/warmup", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("warmup done")
+	})
 	router.HandleFunc("/ping", middlewares.Ping)
 	router.HandleFunc("/getAllShows", controller.GetAllShows)
 	//router.HandleFunc("/searchShows", melishows.ReadSomething).Methods("POST")
