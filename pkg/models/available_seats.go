3157e92f6b1c6a426661fa126b044c1463dd8cbf
maticairo <maticairo@gmail.com>
2022-07-13 16:48:54 -0300

    fix model interpretation

--- pkg/models/available_seats.go
+++ pkg/models/available_seats.go
@@ -3,5 +3,5 @@ package models
 type AvailableSeats struct {
 	Show     string        `json:"show"`
 	Function string        `json:"function"`
-	Seats    []SeatPricing `json:"seats"`
+	Seats    []SeatPricing `json:"available_seats"`
 }
